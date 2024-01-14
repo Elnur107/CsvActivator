3bd67343a460ab55de180f0d11d93c7810402990
AkberliElnur <126491959+Elnur107@users.noreply.github.com>
2024-01-14 22:00:09 +0400

    Add files via upload

---
+++ Activator/Models/Class1.cs
@@ -0,0 +1,52 @@
+﻿namespace Activator.Models
+{
+    public abstract class BaseInfo
+    {
+        public abstract override string ToString();
+    }
+
+    public class Category : BaseInfo
+    {
+        public string Type1 { get; set; }
+        public string Type2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"Category: Type1 - {Type1}, Type2 - {Type2}";
+        }
+    }
+
+    public class Employee : BaseInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee: FirstName - {FirstName}, LastName - {LastName}";
+        }
+    }
+
+    public class Order : BaseInfo
+    {
+        public string OrderId { get; set; }
+        public string CompanyName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Order: OrderId - {OrderId}, CompanyName - {CompanyName}";
+        }
+    }
+
+    public class Product : BaseInfo
+    {
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string UnitPrice { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product: ProductName - {ProductName}, QuantityPerUnit - {QuantityPerUnit}, UnitPrice - {UnitPrice}";
+        }
+    }
+}
---
+++ Activator/Program.cs
@@ -0,0 +1,90 @@
+﻿using Activator.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Activator
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string projectPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string folderPath = Path.Combine(projectPath, "Folder");
+
+            Dictionary<string, List<object>> objectLists = new Dictionary<string, List<object>>();
+
+            try
+            {
+                using (var reader = new StreamReader(Path.Combine(folderPath, "Okecsv.csv")))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+
+                        try
+                        {
+                            ProcessCsvLine(line, objectLists);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error processing CSV line: {ex.Message}");
+                        }
+                    }
+                }
+
+                PrintObjectLists(objectLists);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading CSV file: {ex.Message}");
+            }
+        }
+
+
+        private static void ProcessCsvLine(string line, Dictionary<string, List<object>> objectLists)
+        {
+            var values = line.Split(',');
+            string className = values[0].Trim();
+            int counter = 1;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string fullClassName = "Activator.Models." + className;
+            Type? type = assembly.GetType(fullClassName);
+
+            if (type != null)
+            {
+                object obj = System.Activator.CreateInstance(type);
+
+                foreach (var prop in obj.GetType().GetProperties())
+                {
+                    if (counter == values.Length) continue;
+
+                    Type propType = prop.PropertyType;
+                    object propValue = Convert.ChangeType(values[counter], propType);
+                    prop.SetValue(obj, propValue);
+
+                    counter++;
+                    if (!objectLists.ContainsKey(className))
+                    {
+                        objectLists[className] = new List<object>();
+                    }
+
+                    objectLists[className].Add(obj);
+                }
+            }
+        }
+
+        private static void PrintObjectLists(Dictionary<string, List<object>> objectLists)
+        {
+            foreach (var kvp in objectLists)
+            {
+                Console.WriteLine($"Class --> {kvp.Key}:");
+                Console.WriteLine(kvp.Value.FirstOrDefault()?.ToString());
+                Console.WriteLine();
+            }
+        }
+    }
+}
